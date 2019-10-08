04653bb5fc9f7e497a196ebaaba7c51094f5eab3
Tobias Wich <tobias.wich@electrologic.org>
2019-10-09 01:10:07 +0200

    Further progress of the MQTT module

--- controller/canphone/controller.py
+++ controller/canphone/controller.py
@@ -1,13 +1,19 @@
- # localhost:1883
- # mosquitto_pub -t /baresip/command/ -m '{"command":"hangup"}'                                                                                                                                                                                                                           ✔  4814  22:08:15
- # mosquitto_pub -t /baresip/command/ -m '{"command":"accept"}'
+from .mqtt import MqttController, EventType
+import json
+import logging
 
-from .mqtt import MqttController
+
+log = logging.getLogger("canphone")
+
 
 class CanController:
 
     def __init__(self):
         self.mqtt = MqttController()
+        # callback shortcuts
+        self.cb1 = lambda evt, msg: self.__waitingForCall__(evt, msg)
+        # register first callback
+        self.mqtt.setCallback(self.cb1)
 
     def start(self):
         self.mqtt.connect("localhost")
@@ -15,3 +21,16 @@ class CanController:
     def stop(self):
         self.mqtt.disconnect()
         
+    def dial(self, phoneNumber: str):
+        msgObj = {"command": "dial", "params": phoneNumber}
+        msgStr = json.dumps(msgObj)
+        self.mqtt.send_command(msgStr)
+
+    # Callbacks interacting with the softphone
+
+    def __waitingForCall__(self, evt: EventType, msg):
+        if evt is EventType.CALL_INCOMING:
+            log.info("Accepting incoming call.")
+            msgObj = {"command": "accept"}
+            msgStr = json.dumps(msgObj)
+            self.mqtt.send_command(msgStr)
--- controller/canphone/mqtt.py
+++ controller/canphone/mqtt.py
@@ -1,13 +1,30 @@
 from paho.mqtt.client import Client, MQTTMessage, MQTT_ERR_SUCCESS
 import logging
+import json
+from json import JSONDecodeError
+from enum import Enum, auto
+from typing import Dict, Callable, Optional
+import threading
+
 
 log = logging.getLogger("canphone")
+
+
+class EventType(Enum):
+    REGISTER_OK = auto()
+    CALL_RINGING = auto()
+    CALL_ESTABLISHED = auto()
+    CALL_CLOSED = auto()
+    CALL_INCOMING = auto()
+
+EventCallback = Callable[[EventType, Dict], None]
 
 class MqttController:
     def __init__(self):
         self.client = Client()
         self.client.on_connect = self.__on_connect
         self.client.on_message = self.__on_message
+        self.evtCallback: Optional[EventCallback] = None
 
     def connect(self, host, port=1883, keepalive=60):
         log.info("Trying to connect MQTT client.")
@@ -18,17 +35,45 @@ class MqttController:
         self.client.loop_stop()
         self.client.disconnect()
 
+    def setCallback(self, callback: EventCallback):
+        self.evtCallback = callback
+
+    def delCallback(self):
+        self.evtCallback = None
+
     def __on_connect(self, client, userdata, flags, rc):
         log.info("MQTT client connected, registering subscriptions.")
         self.client.subscribe("/baresip/event")
 
     def __on_message(self, client, userdata, msg: MQTTMessage):
-        log.info("MQTT message received.")
+        log.info("MQTT message received for path=%s.", msg.topic)
         log.debug("payload=%s", msg.payload)
-        
+
+        # parse message
+        try:
+            msgObj = json.loads(msg.payload)
+            evtType = EventType[msgObj["type"]]
+
+            # notify respective callback
+            cb = self.evtCallback
+            if cb is not None:
+                log.debug("Calling event callback in a thread.")
+                t = threading.Thread(target=lambda: cb(evtType, msgObj))
+                t.start()
+            else:
+                log.debug("No callback registered.")
+        except JSONDecodeError:
+            log.error("Received invalid JSON message.")
+        except KeyError:
+            log.warn("Received unhandled type code or type code is missing.")
 
     def send_command(self, msg):
+        log.debug("Trying to send message %s to phone.", msg)
         info = self.client.publish("/baresip/command/", msg)
+        log.debug("Waiting for publish")
+        log.debug(info.rc)
         info.wait_for_publish()
         if info.rc != MQTT_ERR_SUCCESS:
             log.error("Failed to publish message")
+        else:
+            log.debug("Message sent successfully.")
