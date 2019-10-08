0ab9d5cf5045aac9331d0cca818da846f691851a
Tobias Wich <tobias.wich@electrologic.org>
2019-10-09 01:10:26 +0200

    Add deps and files for GPIO and alsamixer functionality

---
+++ controller/canphone/hwcontrols.py
@@ -0,0 +1,7 @@
+import gpiozero
+
+# implement controller where it is possible to
+# - control leds and beeper for incoming calls
+# - read status of the hangup switch
+# - register callback for getting the hangup status of the can
+# - register callback for Push2Talk button
---
+++ controller/canphone/mixer.py
@@ -0,0 +1,6 @@
+import alsaaudio
+
+# documentation for alsa: https://larsimmisch.github.io/pyalsaaudio/libalsaaudio.html
+
+# implement class capable of toggle mute of the speaker and microphone
+# initial state of the mixer shall be set to mic mute, speaker not mute
