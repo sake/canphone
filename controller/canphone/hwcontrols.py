78e5420f6baa34c2851c1d24ad1e7fd5fb9efe44
Val++le <v.kummert@drmnt.de>
2019-10-16 16:19:29 +0200

    First init, add some simple functions

--- controller/canphone/hwcontrols.py
+++ controller/canphone/hwcontrols.py
@@ -1,7 +1,34 @@
-import gpiozero
+import thread
+from gpiozero import LED, Button
+from signal import pause
+from time import sleep
 
-# implement controller where it is possible to
-# - control leds and beeper for incoming calls
-# - read status of the hangup switch
-# - register callback for getting the hangup status of the can
-# - register callback for Push2Talk button
+led = LED(17)
+button = Button(3)
+isItBlinking = False
+
+def startBlinking():
+    isItBlinking = True
+
+def stopBlinking():
+    isItBlinking = False
+
+def blinking():
+    while isItBlinking == True:
+        led.on
+        sleep(1) 
+        led.off
+        sleep(1) 
+
+def buttonListener():
+    while True:
+        button.when_pressed = # add function call push to talk
+        button.when_released = # add function call end push to talk
+        sleep(1)
+
+def main():
+    thread.start_new_thread(buttonListener, ())
+    thread.start_new_thread(blinking, ())
+
+if __name__ == '__main__':
+    main()
