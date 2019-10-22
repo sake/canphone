a9c325b5361c311cb80b5308f9a492e3b2e7ac23
Val++le <v.kummert@drmnt.de>
2019-10-22 23:08:37 +0200

    first refactor init

--- controller/canphone/hwcontrols.py
+++ controller/canphone/hwcontrols.py
@@ -1,34 +1,63 @@
-import thread
-from gpiozero import LED, Button
+import threading
+from gpiozero import LED, Button, Device, Pin
 from signal import pause
 from time import sleep
+from enum import Enum, auto
 
-led = LED(17)
-button = Button(3)
-isItBlinking = False
+class CanState(Enum):
+    LIFTED = auto()
+    HUNGUP = auto()
 
-def startBlinking():
-    isItBlinking = True
+class PttState(Enum):
+    PRESSED = auto()
+    RELEASED = auto()
 
-def stopBlinking():
-    isItBlinking = False
+class PhoneControls:
+    def __init__(self,  callbPush, callbHang, led = LED(17), pushToTalkButton = Device.pin_factory.pin(18), hangUpButton = Device.pin_factory.pin(19), isItBlinking = False):
+        # define properties
+        self.led = led
+        self.isItBlinking = isItBlinking
+        self.pushToTalkButton = pushToTalkButton
+        self.hangUpButton = hangUpButton
+        # configure hardware
+        self.__configPinHandler__(pushToTalkButton, self.__pttCallback__)
+        self.__configPinHandler__(hangUpButton, self.__canCallback__)
+        self.cbCan = callbHang
+        self.cbPtt = callbPush
 
-def blinking():
-    while isItBlinking == True:
-        led.on
-        sleep(1) 
-        led.off
-        sleep(1) 
+    def __configPinHandler__(self, button: Pin, cb):
+        button.edges = "both"
+        button.input_with_pull("up")
+        button.bounce = 250
+        button.when_changed = cb
 
-def buttonListener():
-    while True:
-        button.when_pressed = # add function call push to talk
-        button.when_released = # add function call end push to talk
-        sleep(1)
+    def __canCallback__(self, ticks, state):
+        # TODO: convert state
+        if state == 1:
+            enumState = CanState.HUNGUP
+        else:
+            enumState = CanState.LIFTED
+        self.cbCan(enumState)
 
-def main():
-    thread.start_new_thread(buttonListener, ())
-    thread.start_new_thread(blinking, ())
+    def __pttCallback__(self, ticks, state):
+        # TODO: convert state
+        if state == 1:
+            enumState = PttState.PRESSED
+        else:
+            enumState = PttState.RELEASED
+        self.cbCan(enumState)
+    
+    def startBlinking(self):
+        self.blinkThread = threading.Thread(target=self.__blinking__)
 
-if __name__ == '__main__':
-    main()
+    def stopBlinking(self):
+        if self.blinkThread != None:
+            self.blinkThread._stop()
+            self.blinkThread = None
+        
+    def __blinking__(self):
+        while True:
+            self.led.on
+            sleep(1) 
+            self.led.off
+            sleep(1)
